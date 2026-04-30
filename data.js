c8c52c2e94d810c84e5c4d4b13655b36b2a71276
Boitumelo-N <BoitumeloNgobene@gmail.com>
2026-04-30 09:25:41 +0200

    Update data.js

---
+++ data.js
@@ -0,0 +1,20 @@
+const candidates = [
+  {
+    id: 1,
+    name: "Boitumelo",
+    skills: ["Marketing", "Content Creation", "Social Media"],
+    status: "Completed Internship",
+    location: "Johannesburg",
+    cv: "cv/boitumelo.pdf",
+    image: "assets/images/user1.jpg"
+  },
+  {
+    id: 2,
+    name: "Thabo",
+    skills: ["IT Support", "Networking"],
+    status: "Currently Doing Internship",
+    location: "Pretoria",
+    cv: "cv/thabo.pdf",
+    image: "assets/images/user2.jpg"
+  }
+];
